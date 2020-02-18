8e1fc8f41ac19c7281d23539875642949f5afd74
PotatoTax <conor@johngregg.org>
2020-02-17 21:56:14 -0600

    MeetResultsController now uses PathVariables

--- src/main/java/com/crossrank/api/MeetResultsController.java
+++ src/main/java/com/crossrank/api/MeetResultsController.java
@@ -26,18 +26,14 @@ package com.crossrank.api;
 
 import com.crossrank.backend.datatypes.Race;
 import com.crossrank.backend.serialization.MeetResultsSerializer;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MeetResultsController {
 
     @CrossOrigin(origins = "http://localhost:9000")
-    @GetMapping("/api/meet/results")
-    public Race race(@RequestParam(required = false, defaultValue = "363847") int raceId,
-                     @RequestParam(required = false, defaultValue = "680009") int resultsId) {
+    @GetMapping("/api/meet/results/{resultsId}")
+    public Race race(@PathVariable int resultsId) {
         return MeetResultsSerializer.LoadMeetResult(resultsId);
     }
 }
